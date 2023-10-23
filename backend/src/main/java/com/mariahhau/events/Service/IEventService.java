829214a11eef8c1e4c6fa832a58f2c97a67e020e
mariahhau <135694401+mariahhau@users.noreply.github.com>
2023-10-23 10:59:25 +0300

    Create IEventService.java

---
+++ backend/src/main/java/com/mariahhau/events/Service/IEventService.java
@@ -0,0 +1,24 @@
+package com.mariahhau.events.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import com.mariahhau.events.Database.Documents.Event;
+
+public interface IEventService {
+
+    List<Event> allEvents();
+    
+    Optional<Event> singleEvent(long id);
+
+    Integer registerForEvent(long eventId, long userId, String email);
+
+     Integer registerForEvent(long eventId, String email);
+
+    Integer cancelRegistrationForEvent(long eventId, long userId);
+    
+    Event createEvent(Map<String, String> payload, String username);
+
+
+}
