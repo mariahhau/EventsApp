71d5154bb77e83ebcdb23898fc8f7f9803585a81
mariahhau <135694401+mariahhau@users.noreply.github.com>
2023-10-23 10:59:41 +0300

    Update UserServiceImpl.java

--- backend/src/main/java/com/mariahhau/events/Service/UserServiceImpl.java
+++ backend/src/main/java/com/mariahhau/events/Service/UserServiceImpl.java
@@ -7,14 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
-import com.mariahhau.events.Database.Documents.Event;
 import com.mariahhau.events.Database.Documents.User;
 import com.mariahhau.events.Database.Repositories.UserRepository;
 
 @Service 
 public class UserServiceImpl implements IUserService {
 
-    @Autowired //Spring instantiates this automatically
+    @Autowired
     private UserRepository userRepository;
 
     @Autowired
@@ -22,37 +21,33 @@ public class UserServiceImpl implements IUserService {
 
     @Override
     public List<User> findAllUsers() {
-        System.out.println("UserServiceImpl: findAllUsers");
+        System.out.println("UserService: findAllUsers");
         return userRepository.findAll();
     }
         
     @Override
     public Optional<User> findUserByEmail(String email) {
-        System.out.println("UserServiceImpl: findUserByEmail");
+        System.out.println("UserService: findUserByEmail");
         return userRepository.findUserByEmail(email);
     }  
 
     public Optional<User> findUserByUsername(String username) {
-        System.out.print("UserServiceImpl: findUserByUsername :  ");
+        System.out.print("UserService: findUserByUsername:  " + username);
         System.out.println(userRepository.findUserByUsername(username).toString());
         return userRepository.findUserByUsername(username);
     }  
 
-
-    //Tätä kutsutaan userControllerissa tms.? vrt. Eventin luominen
     
     @Override
     public Integer saveUser(User user) {
-       System.out.println("UserServiceImpl: saveUser");
-       if (findUserByUsername(user.getUsername()).isPresent()) {
+       System.out.println("UserService: saveUser");
+       if (findUserByUsername(user.getUsername()).isPresent()){
         return 1;
        }
        SequenceGeneratorService sequenceGenerator = new SequenceGeneratorService(mt);
        user.setId(sequenceGenerator.generateSequence(User.SEQUENCE_NAME));
        userRepository.save(user);
        return 0;
-
-
 
     }
 
